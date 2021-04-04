9e960281b173055de314c3a7b23a9bb3bcfc097a
Aaron Denny <aaron.denny@wayne.edu>
2021-04-03 21:09:47 -0400

    adding navigation with event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  title: string = 'shopping-list-recipe-book';
+  loadedFeature: string = 'recipes';
+
+  onNavigate(feature: string): void {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  collapsed = true;
+  collapsed: boolean = true;
+  
+  @Output() featureSelected: EventEmitter<string> = new EventEmitter<string>();
   
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(feature: string): void {
+    this.featureSelected.emit(feature);
+  }
 }
