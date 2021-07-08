2068a59da8175ada93a754d7911be78045d0f980
aaronmdenny <aaronmdenny@outlook.com>
2021-07-08 09:17:47 -0400

    using services for pushing data from A to B

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,13 @@
+import { EventEmitter } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
+    // When we add an ingredient to the Ingredient array, other classes do not know about the addition becuase they get
+    // a slice of the array, not the actual array in this class. We create an event for other classes to subscribe to so
+    // they can know when a new ingredient is added and update their ingredients accordingly.
+    ingredientsChanged: EventEmitter<Ingredient[]> = new EventEmitter();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -8,6 +15,7 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 
     getIngredients() {
