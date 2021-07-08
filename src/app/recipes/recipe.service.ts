1ad4c9c1ae5aa0491ee6b93f51d02a65d94e4868
aaronmdenny <aaronmdenny@outlook.com>
2021-07-07 22:02:33 -0400

    managing recipes in a recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,22 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe(
+          'A Test Recipe',
+          'This is simply a test',
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+        ),
+        new Recipe(
+          'Another Test Recipe',
+          'This is simply a test',
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+        )
+    ];
+
+    getRecipes() {
+        // Return a new array of Recipes that is an exact copy of the recipes in this file by using slice().
+        // This prevents modification of the recipes from outside of the class.
+        return this.recipes.slice();
+    }
 }
