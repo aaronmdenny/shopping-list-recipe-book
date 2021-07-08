0df46c095ef67bcdf3374622a220a050849dbe62
aaronmdenny <aaronmdenny@outlook.com>
2021-07-07 22:17:04 -0400

    using a service for cross-component communication

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -12,8 +12,15 @@ import { RecipeService } from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
+  // We can subscribe to an event just like we can subscribe to an Observable.
   ngOnInit(): void {
+    this.recipeService.recipeSelected
+      .subscribe(
+        (recipe: Recipe) => {
+          this.selectedRecipe = recipe;
+        }
+      );
   }
 }
